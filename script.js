a03092109dca1781d48f2b7f879ec6193aae215f
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-25 16:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // Your script here.
+
+  function populateVoices() {
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length === 0) {
+      voicesDropdown.innerHTML = '<option disabled>No voices available</option>';
+      return;
+    }
+    voicesDropdown.innerHTML = voices
+      .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+      .join('');
+  }
+
+  function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    restartSpeech();
+  }
+  function setOption() {
+    msg[this.name] = this.value;
+    if (speechSynthesis.speaking) restartSpeech();
+  }
+
+  function speak() {
+    if (!msg.text.trim()) return alert("Please enter text to speak!");
+    speechSynthesis.cancel();
+    speechSynthesis.speak(msg);
+  }
+  function stop() {
+    speechSynthesis.cancel();
+  }
+  function restartSpeech() {
+    stop();
+    speak();
+  }
+  msg.text = document.querySelector('[name="text"]').value;
+
+  speechSynthesis.addEventListener('voiceschanged', populateVoices);
+  voicesDropdown.addEventListener('change', setVoice);
+  options.forEach(option => option.addEventListener('change', setOption));
+  speakButton.addEventListener('click', speak);
+  stopButton.addEventListener('click', stop);
+
